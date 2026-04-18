264c700f8a01a754808602302b29944625a1a650
cjean <cjeancito121212@gmail.com>
2026-04-18 13:11:05 -0500

    Mario1

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -21,5 +21,26 @@ export const movies: Movie[] = [
  
         ],
 },
+ {
+    id: "2",
+    title: "Super Mario Bros. La película",
+    year: 2023,
+    description: "Mientras trabaja bajo tierra para arreglar una tubería de agua, los plomeros de Brooklyn Mario y su hermano Luigi son transportados por una misteriosa tubería y llegan a un nuevo mundo mágico. Pero cuando los hermanos se separan, Mario se embarca en una búsqueda épica para encontrar a Luigi. Con la ayuda de un residente del Reino Champiñón, Toad y algo de entrenamiento de la gobernante del Reino Champiñón, la Princesa Peach, Mario conocerá su propio poder.",
+    trailer:"youtube:SvJwEiy2Wok",
+    thumbnail: "https://compucalitv.tv/wp-content/uploads/thumbs/c60ecb2ca9a1dc10133ed2d86386e3f9_hd.webp",
+    genre: "Aventura,Animación,Familia,FantasíaC,Comedia",
 
+        opcion: [
+          {
+            title: "Opcion 1",
+            file: "drive:1rqVwXyzXkwiipKbmRtlPzW1qprZpiiqf",
+          },
+          {
+            title: "Opcion 2",
+            file: "drive:1rqVwXyzXkwiipKbmRtlPzW1qprZpiiqf",
+          
+          },
+ 
+        ],
+},
 ];
