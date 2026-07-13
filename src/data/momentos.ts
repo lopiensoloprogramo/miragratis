4e3634739c2068f01e40469817bf682e3f9855eb
cjean <cjeancito121212@gmail.com>
2026-07-13 17:58:58 -0500

    ratasvssss23

--- src/data/momentos.ts
+++ src/data/momentos.ts
@@ -15,7 +15,7 @@ export const momentos = [
     thumbnail: "https://m.media-amazon.com/images/M/MV5BY2VjNmE3ZTctZjAyNS00Yjk0LTg3NGYtNDMwNzIwN2JiNWJjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     year: 2017,
     duration: "6:40",
-    description: "Batalla espectacular en 'The Witcher",
+    description: "Batalla espectacular en 'The Witcher, 'Las Ratas' vs Caza Recompensas.",
     genre: ["Acción"],
     video: "mega:SIYADb4B#f74UDTXpnNVKqThq5DxDxQW7dSgSV3nL_Cn_5dn1-Jo"
   },
