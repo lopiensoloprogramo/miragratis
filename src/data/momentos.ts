79bb835ce820bd3d56e83c36bb6b95387ac24271
cjean <cjeancito121212@gmail.com>
2026-07-15 04:46:19 -0500

    momentoTheBoys

--- src/data/momentos.ts
+++ src/data/momentos.ts
@@ -13,11 +13,21 @@ export const momentos = [
     id: "2",
     title: "Las Ratas vs Cazarecompensas",
     thumbnail: "https://m.media-amazon.com/images/M/MV5BY2VjNmE3ZTctZjAyNS00Yjk0LTg3NGYtNDMwNzIwN2JiNWJjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-    year: 2017,
+    year: 2023,
     duration: "6:40",
     description: "Batalla espectacular en 'The Witcher, 'Las Ratas' vs CazaRecompensas.",
     genre: ["Acción"],
     video: "mega:SIYADb4B#f74UDTXpnNVKqThq5DxDxQW7dSgSV3nL_Cn_5dn1-Jo"
+  },
+      {
+    id: "3",
+    title: "Homelander vs Butcher",
+    thumbnail: "https://images.justwatch.com/poster/319251193/s718/the-boys.jpg",
+    year: 2026,
+    duration: "12:09",
+    description: "Batalla final entre el capitan de los seven y el loco de Butcher.",
+    genre: ["Acción"],
+    video: "mega:zBJwBaKI#lGWWx5ZSGa0J5ZgNhN4HtRAN1eDqawbKxcUBvyQQBq8"
   },
 
 ];
