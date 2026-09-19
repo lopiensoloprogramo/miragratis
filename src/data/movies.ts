dddfdb85cc643415827b00e535c83d11f1e43c09
cjean <cjeancito121212@gmail.com>
2026-09-18 19:33:46 -0500

    28semanas2

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -1234,7 +1234,7 @@ export const movies: Movie[] = [
     languaje:["Español latino"],
     description:"Seis meses después de que la propagación de un virus haya devastado las Islas Británicas, el ejército de los Estados Unidos declara que, vencida la epidemia, ya se puede acometer la reconstrucción del país. Los evacuados regresan entonces al país y las familias se reúnen de nuevo. Sin embargo, el virus aún no ha sido destruido y es más peligroso que nunca.",
     trailer:"youtube:ALlpOHHLtxw",
-    thumbnail: "https://www.zona-leros.com/storage/movies_tumbl/28-semanas-despues-cover-tdw.jpg",
+    thumbnail: "https://elblogdemanu.com/28-semanas-despues-una-secuela-a-la-altura-del-original/poster.webp",
     genre: ["Zombies","Terror","Fantasía","Misterio","Drama"],
     gallery:[],
         opcion: [
