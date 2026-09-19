83fc1a6c2bdf7a4174a9e7eaa7879ac931c9e7b6
cjean <cjeancito121212@gmail.com>
2026-09-19 10:50:20 -0500

    blackphone1

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -1271,4 +1271,26 @@ export const movies: Movie[] = [
         
         
 },
+{
+    id: "52",
+    title: "Telefono Negro (Black Phone)",
+    year: 2022,
+    languaje:["Español latino"],
+    description:"Finney Shaw, un tímido, pero inteligente niño de 13 años es secuestrado por un sádico asesino y atrapado en un aterrador, oscuro y aislado sótano, donde los gritos son de poca utilidad. Cuando un teléfono negro desconectado en la pared comienza a sonar, Finney descubre que puede escuchar a los espíritus de las víctimas anteriores del asesino.",
+    trailer:"youtube:JZB1lFciUrw",
+    thumbnail: "https://static.wikia.nocookie.net/doblaje/images/d/da/El_tel%C3%A9fono_negro_poster.jpg/revision/latest?cb=20220701034454&path-prefix=es",
+    genre: ["Zombies","Terror","Fantasía","Misterio","Drama"],
+    gallery:[],
+        opcion: [
+          {
+            title: "Opcion 1",
+            file: "drive:1NoXrCqAzZRqBHMZLxsV49kAVzxr98uTQ",
+            download:"https://ouo.io/wUtAf5",
+          },
+
+ 
+        ],
+        
+        
+},
 ];
